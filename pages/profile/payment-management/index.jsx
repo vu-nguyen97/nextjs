90e2c412ab2cb2485a265f6fc646493e6af537a4
vu.nguyenvan <vunv@falcongames.com>
2022-02-15 16:41:49 +0700

    Create mockup for payment management component

--- pages/profile/payment-management/index.jsx
+++ pages/profile/payment-management/index.jsx
@@ -1,8 +1,109 @@
-import React from "react";
+import React, { useState } from "react";
 import { ProfileLayout } from "@components";
+import Image from "next/image";
 
 function PaymentManagement() {
-  const childrenEl = <div>Payment Management</div>;
+  const [isShowPaymentMethod, setIsShowPaymentMethod] = useState(false);
+  const [isShowCreditCardDetail, setIsShowCreditCardDetail] = useState(false);
+  const [isShowPaypalDetail, setIsShowPaypalDetail] = useState(false);
+
+  const handleAddPaymentMethod = () => {
+    const arrowEl = document.getElementsByClassName("effect-arrow")?.[0];
+
+    if (arrowEl) {
+      if (!isShowPaymentMethod) {
+        arrowEl.classList.add("open");
+      } else {
+        arrowEl.classList.remove("open");
+      }
+    }
+
+    setIsShowPaymentMethod(!isShowPaymentMethod);
+  };
+
+  const onChangeCheckbox = (isChoosePaypalCB) => {
+    setIsShowCreditCardDetail(!isChoosePaypalCB);
+    setIsShowPaypalDetail(isChoosePaypalCB);
+  };
+
+  const childrenEl = (
+    <div>
+      <h5 className="text-uppercase">Payment Management</h5>
+
+      <div className="mt-3 text-muted">
+        View your payment activity and the current balance of your account. View
+        our{" "}
+        <span>
+          <a href="/">Privacy Policy</a>
+        </span>
+        .
+      </div>
+
+      <br />
+      <div className="h6 mt-2">YOUR PAYMENT METHODS</div>
+      <hr />
+
+      <div className="text-muted">
+        Add or manage payment methods associated with your account.
+      </div>
+
+      <div className="mt-4">
+        <div
+          className="d-flex align-items-center text-info cursor-pointer"
+          onClick={handleAddPaymentMethod}
+        >
+          <div className="font-size-14 ">ADD A PAYMENT METHOD</div>
+          <Image
+            src="/icons/arrow-down.svg"
+            className="effect-arrow"
+            width="40"
+            height="7"
+          />
+        </div>
+      </div>
+
+      {isShowPaymentMethod && (
+        <div>
+          <div className="d-flex align-items-center mt-2 bg-gray rounded-3 p-3 cursor-pointer">
+            <input
+              type="radio"
+              id="credit_card"
+              value="credit_card"
+              className="radio-lg me-3"
+              checked={isShowCreditCardDetail}
+              onChange={() => onChangeCheckbox(false)}
+            />
+
+            <div className="px-3 border-gray rounded-3">
+              <i className="h3 m-0 bi bi-credit-card-fill"></i>
+            </div>
+            <span className="ms-3">Credit Card</span>
+          </div>
+
+          <div className="d-flex align-items-center mt-3 bg-gray rounded-3 p-3 cursor-pointer">
+            <input
+              type="radio"
+              id="paypal"
+              value="paypal"
+              className="radio-lg me-3"
+              checked={isShowPaypalDetail}
+              onChange={() => onChangeCheckbox(true)}
+            />
+
+            <div className="d-flex align-items-center py-1 px-3 border-gray rounded-3">
+              <Image
+                src="/icons/paypal.png"
+                alt="paypal"
+                width="28"
+                height="28"
+              />
+            </div>
+            <span className="ms-3">Paypal</span>
+          </div>
+        </div>
+      )}
+    </div>
+  );
 
   return <ProfileLayout children={childrenEl} />;
 }
--- src/components/profile/layout.tsx
+++ src/components/profile/layout.tsx
@@ -1,23 +1,37 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Header, Footer } from "@components";
 import { Container } from "react-bootstrap";
 import styles from "@styles/pages/profile.module.scss";
 import classNames from "classnames";
 import Link from "next/link";
+import { useRouter } from "next/router";
 
 const TABS = [
-  { url: "", name: "General", icon: "person-fill" },
-  { url: "communications", name: "Communications", icon: "bell-fill" },
+  { id: 0, url: "", name: "General", icon: "person-fill" },
+  { id: 1, url: "communications", name: "Communications", icon: "bell-fill" },
   {
+    id: 2,
     url: "payment-management",
     name: "Payment management",
     icon: "credit-card-fill",
   },
-  { url: "transactions", name: "Transactions", icon: "bag-check-fill" },
-  { url: "history", name: "History", icon: "clock-history" },
+  { id: 3, url: "transactions", name: "Transactions", icon: "bag-check-fill" },
+  { id: 4, url: "history", name: "History", icon: "clock-history" },
 ];
 
 export const ProfileLayout: React.FC = ({ children }: any) => {
+  const router = useRouter();
+  const [activedTab, setActivedTab] = useState(0);
+
+  useEffect(() => {
+    const slug = router.route?.split("/")?.slice(-1)[0];
+    const activedTab = TABS.find((el) => el.url === slug);
+
+    if (activedTab) {
+      setActivedTab(activedTab.id);
+    }
+  }, []);
+
   return (
     <div className="d-flex flex-column min-vh-100">
       <Header />
@@ -25,25 +39,32 @@ export const ProfileLayout: React.FC = ({ children }: any) => {
       <div className="py-4 flex-grow-1">
         <Container>
           <div className="d-flex">
-            <div className={styles.sideBar}>
-              {TABS.map((tab, id) => (
-                <Link href={`/profile/${tab.url}`} key={id}>
-                  <div
-                    className={classNames(
-                      "d-flex align-items-center",
-                      styles.tab
-                    )}
-                  >
-                    <i className={`h5 m-0 bi bi-${tab.icon}`}></i>
-                    <div className="ms-2 text-uppercase font-size-12">
-                      {tab.name}
+            <div>
+              <div className={styles.sideBar}>
+                {TABS.map((tab) => (
+                  <Link href={`/profile/${tab.url}`} key={tab.id}>
+                    <div
+                      className={classNames(
+                        "d-flex align-items-center fw-bold",
+                        styles.tab,
+                        { [styles.activedTab]: activedTab === tab.id }
+                      )}
+                    >
+                      <i className={`h5 m-0 bi bi-${tab.icon}`}></i>
+                      <div className="ms-2 text-uppercase font-size-11 mt-1">
+                        {tab.name}
+                      </div>
                     </div>
-                  </div>
-                </Link>
-              ))}
+                  </Link>
+                ))}
+              </div>
             </div>
 
-            <div className="flex-grow-1 ms-3 p-4 border">{children}</div>
+            <div className="w-100">
+              <div className={classNames("ms-3 border", styles.content)}>
+                {children}
+              </div>
+            </div>
           </div>
         </Container>
       </div>
